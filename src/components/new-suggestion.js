98b2d396024bc952654f0ae4ab8538eb5e5f8dee
John Varghese <lookitup4me@gmail.com>
2019-09-30 17:52:07 -0700

    feat: add new-suggetions css and style the new suggestion area. add name input as well

--- src/components/new-suggestion.js
+++ src/components/new-suggestion.js
@@ -1,27 +1,48 @@
 import React from 'react'
+import './new-suggestion.css'
 
 const newSuggestion = (props) => {
   return (
-    <div>
-      <form onSubmit={props.handleSubmitNewSuggestion}>
-        <div>
+    <div className='suggestion-container'>
+      <div className="the-reason">
+        For the 2020 AWS Community Day, we want to tailor the talks to
+        what the audience really wants to hear about. So we want your
+        suggestions on what the talks should be about.
+        <p>What topics would you like to hear about at the NEXT talk?</p>
+      </div>
+
+      <form className="suggestion-form" onSubmit={props.handleSubmitNewSuggestion}>
+        <div className="suggestion-box-label" >
           <label htmlFor="suggestion">Your topic suggestion</label>
         </div>
         <div>
-          <textarea name="suggestion" id="suggestion" cols="60" rows="5" placeholder="Your suggestion here please" required></textarea>
+          <textarea name="suggestion" id="suggestion"
+            rows="5" className="suggestion"
+            placeholder="Your suggestion here please" required></textarea>
+        </div>
+        <div className="suggestion-box-label" >
+          <label htmlFor="email">Email (this will not be visible to others)</label>
         </div>
         <div>
-          <label htmlFor="email">Email</label>
+          <input type="email" id="email" placeholder="Your email" required
+            className="suggester-email" />
+        </div>
+        <div className="suggestion-box-label" >
+          <label htmlFor="user-full-name">Your name please</label>
         </div>
         <div>
-          <input type="email" id="email" placeholder="Your email" required />
+          <input type="text" id="user-full-name" placeholder="Your name" required
+            className="suggester-name" />
         </div>
+        <p></p>
         <div>
           <button type="submit">Thats what I want </button>
         </div>
       </form>
+
+
     </div>
   )
 }
 
-export default newSuggestion; 
+export default newSuggestion;
